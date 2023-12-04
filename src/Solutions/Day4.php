31cde6eaf638e92d2fb6274fad37724d8c5b92de
Freyr H <freyr.helga@gmail.com>
2023-12-04 23:51:56 +0000

    Day 4 Solve Change form array to Ds\Vector

--- src/Solutions/Day4.php
+++ src/Solutions/Day4.php
@@ -8,6 +8,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+use Ds\Vector as vec;
+
 use frhel\adventofcode2023php\Tools\Timer;
 
 
@@ -50,6 +52,8 @@ class Day4 extends Command
         // ====================================================================== //
         // Start the timer
         $overallTimer = new Timer();
+
+        // Solve both parts at the same time. See solve() docblock for more info
         $solution = $this->solve($data);
 
         // Right answer: 15205
@@ -73,16 +77,19 @@ class Day4 extends Command
      * Part 2: The total number of cards and card copies in the game
      */
     protected function solve($cards) {
+        $cards = new vec($cards);
         $point_sum = 0;
         $total_cards = 0;
 
         foreach ($cards as $key => $card) {
-            $card = $cards[$key];
-            $winners = $cards[$key]['winners'];
-            $numbers = $cards[$key]['numbers'];
+            $winners = $card[0];
+            $numbers = $card[1];
 
             // Count the wins for this card once
-            $win_count = count(array_intersect($winners, $numbers));
+            $win_count = 0;
+            foreach ($winners as $winner)
+                if ($numbers->contains($winner))
+                    $win_count++;
             
             // If the card has more than one win, it's worth 2^(wins-1) points
             // otherwise it's worth $win_count points (1 or 0)
@@ -91,15 +98,15 @@ class Day4 extends Command
             // For every copy of this card, add the number of wins to the count of the next $j cards
             // That way we keep track of how many cards are in the game as we go
             for ($j = 1; $j <= $win_count; $j++) {
-                $next_key = $key + $j;
-                if ($next_key >= count($cards)) { break; } // We're at the end of the game, stop counting
-                $cards[$next_key]['count'] += $card['count'];
+                (int) $tkey = $key + $j;
+                if ($tkey >= $cards->count()) break;
+                $cards[$tkey]->set(2, $cards[$tkey][2] + $card[2]);
             }
 
             // Add the points for this card to the total sum of points
             $point_sum += $points; 
             // Add the number of copies of this card to the total number of cards
-            $total_cards += $card['count'];
+            $total_cards += $card[2];
         }
 
         // Return both answers at the same time
@@ -110,39 +117,32 @@ class Day4 extends Command
      * Parses the input file into an array of cards with winners, numbers, and copy count
      * 
      * @param string $data
-     * @return array
+     * @return Ds\Vector
      */
     protected function parse_input($data) {        
         $data = preg_split('/\r\n|\r|\n/', $data);
-
-        $data_out = [];
-        
+        $data_out = new vec();
+                
         foreach ($data as $key => $card) {
             $card = explode(' | ', explode(': ', $card)[1]);
-            $data_out[$key]['winners'] = explode(' ', $card[0]);
-            $data_out[$key]['numbers'] = explode(' ', $card[1]);
 
-            // Saving the number of copies of this card for later
-            $data_out[$key]['count'] = 1;
-
-            // Convert the numbers to integers and trim whitespace
-            foreach ($data_out[$key]['numbers'] as $idx => $number) {
-                if ($number === '' || $number === ' ') {
-                    unset($data_out[$key]['numbers'][$idx]);
-                    continue;
-                }
-                $data_out[$key]['numbers'][$idx] = (int) trim($number);
-            }
-            foreach ($data_out[$key]['winners'] as $idx => $winner) {
-                if ($winner === '' || $winner === ' ') {
-                    unset($data_out[$key]['winners'][$idx]);
-                    continue;
-                }
-                $data_out[$key]['winners'][$idx] = (int) trim($winner);
-            }
-
+            $data_out->push(new vec([
+                new vec($this->parse_numbers(explode(' ', $card[0]))),
+                new vec($this->parse_numbers(explode(' ', $card[1]))),
+                1
+            ]));
         }
         return $data_out;
     }
 
+    protected function parse_numbers($data) {
+        foreach ($data as $idx => $number) {
+            if ($number === '' || $number === ' ') {
+                unset($data[$idx]);
+                continue;
+            }
+            $data[$idx] = (int) trim($number);
+        }
+        return $data;
+    }
 }
