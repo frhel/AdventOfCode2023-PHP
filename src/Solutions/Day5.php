50baa2def9bb74ac802632f8fc59450e67de65a1
Freyr Helgason <freyr.helga@gmail.com>
2023-12-07 19:22:02 +0000

    Update the parser with ranges

--- src/Solutions/Day5.php
+++ src/Solutions/Day5.php
@@ -7,11 +7,8 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Amp\Future;
-use Amp\Parallel\Worker;
 
 use frhel\adventofcode2023php\Tools\Timer;
-use frhel\adventofcode2023php\Tools\RunTask;
 
 class Day5 extends Command
 {
@@ -43,13 +40,13 @@ class Day5 extends Command
         $data_full = file_get_contents($this->dataFile);
         $data_example = file_get_contents($this->exampleFile);
 
-        // Default to example data. Just comment out this line to use the real data.
-        // $this->ex = 1;
-        $data = $this->parse_input($this->ex === 1 ? $data_example : $data_full);
-
         // ====================================================================== //
         // ============================ Start Solving =========================== //
         // ====================================================================== //
+        // Default to example data. Just comment out this line to use the real data.
+        $this->ex = 1;
+        $data = $this->parse_input($this->ex === 1 ? $data_example : $data_full);
+
         // Start the timer
         $overallTimer = new Timer();
 
@@ -66,61 +63,28 @@ class Day5 extends Command
         return Command::SUCCESS;
     }
 
-    protected function solve($data) {     
-        $timer = new Timer();
-        $part1 = INF;
-        $part2 = INF;
+    protected function solve($data) { 
+        $part1 = 0;
+        $part2 = 0;
 
         $seeds = $data['seeds'];
         $maps = $data['maps'];
+
+        $pairs = array_map(fn($x) => [(int)$x, (int) $x], $seeds);
+        print_r($pairs);
+
+        foreach ($maps as $map) {
+            $new_pairs = [];
+            foreach ($map as $pair) {
+                
+            }
+        }
         
+        $pairs = $this->generate_pairs($seeds);
+        print_r($pairs);
 
-        $part1exec = [];
-        $part1exec[] = Worker\submit(new RunTask([$seeds, $maps, 0, 0], 1, 0, 0));
-
-        // Each submission returns an Execution instance to allow two-way
-        // communication with a task. Here we're only interested in the
-        // task result, so we use the Future from Execution::getFuture()
-        $part1task = Future\await(array_map(
-            fn (Worker\Execution $e) => $e->getFuture(),
-            $part1exec,
-        ));
-
-        foreach ($part1task as $part1task) {
-            echo $part1task . PHP_EOL;
-            $part1 = min($part1, (int) $part1task);
-        }
-
-        echo 'Part 1 Time: ' . $timer->stop() . PHP_EOL;
-
-        $part2exec = [];
-        $ranges = $this->generate_pairs($seeds);
-        $ranges_total = 0;
-        foreach($ranges as $key=>$range) {
-            $ranges_total += $range[1] - $range[0];
-        }
-
-        foreach ($ranges as $key => $range) {
-            echo 'Starting range: ' . $key . PHP_EOL;
-            $part2exec[] = Worker\submit(new RunTask([$range, $maps, 0, 0], 2, $key, $ranges_total));
-        }
-
-        $part2task = Future\await(array_map(
-            fn (Worker\Execution $e) => $e->getFuture(),
-            $part2exec,
-        ));
-        foreach ($part2task as $part2task) {
-            echo $part2task . PHP_EOL;
-            $part2 = min($part2, (int) $part2task);
-        }
-
-        echo 'Part 2 Time: ' . $timer->stop() . PHP_EOL;
-
-        //echo $this->find_destination(14) . PHP_EOL;
-
-        // Don't ask why the -1 is needed. I don't know. I don't want to know. I don't care.
-        // I just want to go to bed.
-        return [$part1, $part2 - 1];
+        print_r($maps);
+        return [$part1, $part2];
     }
 
     protected function generate_pairs($seeds) {
@@ -156,8 +120,11 @@ class Day5 extends Command
                 continue;
             }
             $current_map[] = explode(' ', $line);
+
+            $processed['dest'] = $current_map[0][0] + $current_map[0][2];
+            $processed['src'] = $current_map[0][0];
         }
-        $maps[] = $current_map;
+        $maps[] = $processed;
 
         return ['seeds' => $seeds, 'maps' => $maps];
     }
