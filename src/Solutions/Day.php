58091c91dd802f6a0fd22cb36c96ea3eb4954211
Freyr <freyrh@gmail.com>
2023-12-16 10:00:01 +0100

    Solve Day 13

--- src/Solutions/Day.php
+++ src/Solutions/Day.php
@@ -8,7 +8,7 @@ use frhel\adventofcode2023php\Tools\Prenta;
 use frhel\adventofcode2023php\Tools\Utils;
 
 class Day
-{   
+{
     public $ex;
     public array $data;
     function __construct(private int $day, $bench = 0, $ex = 0) {
@@ -17,44 +17,44 @@ class Day
         $this->day = $day;
         $data = $this->parse_input($this->load_data($day, $ex)); $this->data = $data;
         $this->data = $data;
-        if ($bench < 0) return; // Don't run the actual solution if we're benchmarking
+        if ($bench < 0) return; // The benchmarking code will call solve() directly if activated
 
         $left_pad = str_repeat(' ', strlen((string)$day));
-        echo PHP_EOL;    
-        //Prenta::std($left_pad."                              ", 'normal', 'red');
-        Prenta::std("       Solving day nr. $day       ", 'white', 'grey');        
         echo PHP_EOL;
+        //Prenta::std($left_pad."                              ", 'normal', 'red');
+        Prenta::std("       Solving day nr. $day       ", 'white', 'cyan');
+        echo PHP_EOL;
+
         $overallTimer = new Timer();
         // Solve both parts at the same time. See solve() docblock for more info
-        $solution = $this->solve($data);        
+        $solution = $this->solve($data);
+
+
         Prenta::label('Execution time', $overallTimer->stop(),  'cyan');
         Prenta::answer($solution[0], 1);
         Prenta::answer($solution[1], 2);
         echo PHP_EOL;
-        Prenta::std($left_pad."                              ", 'normal', 'grey');
+        Prenta::std($left_pad."                              ", 'normal', 'cyan');
         echo PHP_EOL;
 
         Utils::bench($day, $data, $bench); // 0 runs to turn off
     }
-    
+
 
     /**
      * Solves the problem. Needs to be public so we can call it from the benchmarking code
-     * 
+     *
      * @param array $data The data to solve
      * @return array The solution to the problem in the form of [part1, part2]
      */
-    public function solve($data) {
-        $part1 = 0;
-        $part2 = 0;
-        
+    public function solve($data, $part1 = 0, $part2 = 0) {
 
         return [&$part1, &$part2];
     }
 
-    protected function parse_input($data) {        
+    protected function parse_input($data) {
         $data = preg_split('/\r\n|\r|\n/', $data);
-        
+
 
         return $data;
     }
--- src/Solutions/Day13.php
+++ src/Solutions/Day13.php
@@ -15,8 +15,8 @@ use frhel\adventofcode2023php\Tools\Utils;
 
 class Day13 extends Day
 {
-    function __construct(private int $day, $bench = 0, $ex = 0) {
-        $ex = 1;
+    function __construct(private int $day, $bench = 1000, $ex = 0) {
+        // $ex = 1;
         parent::__construct($day, $bench, $ex);
     }
 
@@ -27,111 +27,47 @@ class Day13 extends Day
      * @param array $data The data to solve
      * @return array The solution to the problem in the form of [part1, part2]
      */
-    public function solve($data) {
+    public function solve($data, $part1 = 0, $part2 = 0) {
         $patterns = $this->parse_input($this->load_data($this->day, $this->ex)); $this->data = $data;
 
-        $part1 = 0;
-        $part2 = 0;
-
-        $maps = file_get_contents(__DIR__ . "/../../data/day_13");
-        $maps = explode("\n\n", trim($maps));
-
-        $result = $part1 = $part2 = 0;
-
-        // foreach ($maps as $m => $map)
-        // {
-        //     $map = explode("\n", $map);
-        //     $map = array_map(fn($r)=>str_split(str_replace(["#", "."], [1, 0], $r)), $map);
-
-        //     foreach (["H" => 100, "V" => 1] as $reflect => $mult)
-        //     {
-        //         if ($reflect == "V") $map = array_map(null, ...$map);
-
-        //         for ($h = count($map), $r = 0; $r < $h - 1; $r++)
-        //         {
-        //             $diff = 0;
-        //             for ($i = 0; $r - $i >= 0 && $r + 1 + $i < $h; $i++)
-        //             {
-        //                 $lhs = bindec(implode($map[$r - $i]));
-        //                 $rhs = bindec(implode($map[$r + 1 + $i]));
-        //                 if (($diff += substr_count(decbin($lhs ^ $rhs), "1")) > 1) break;
-        //             }
-        //             if ($diff == 0) $part1 += $mult * ($r + 1);
-        //             if ($diff == 1) $part2 += $mult * ($r + 1);
-        //         }
-        //     }
-        // }
-        // return [$part1, $part2];
-
         foreach ($patterns as $pattern) {
-            $rows = $pattern['rows'];
-            $cols = $pattern['cols'];
-
-            [$p1r, $p2r] = $this->find_mirror_pattern($rows);
-            [$p1c, $p2c] = $this->find_mirror_pattern($cols);
-            $part1 += $p1r > 0 ? $p1r * 100 : $p1c;
-            $part2 += $p2r > 0 ? $p2r * 100 : $p2c;
+            [$p1, $p2] = $this->find_mirror_pattern($pattern);
+            $part1 += $p1;
+            $part2 += $p2;
         }
 
         return [$part1, $part2];
     }
 
-    protected function find_mirror_pattern($lines, $smudge = false) {
-        $potential = $this->potential_mirrors($lines, $smudge);
+    protected function find_mirror_pattern($pattern, $dir = 0, $part1 = 0, $part2 = 0) {
+        // $dir = 0 => rows, 1 => cols
+        $lines = $pattern[$dir];
 
-        [$part1, $part2] = [0, -1];
-        foreach ($potential as $idx) {
-            $part1 = $part1 < 1 ? 0 : $part1;
-            $part2 = $part2 < 1 ? -1 : $part2;
-            $offset = 0;
-            while ($idx - $offset - 1 >= 0 && $idx + $offset < count($lines)) {
-                [$l1, $l2] = [$lines[$idx - $offset - 1], $lines[$idx + $offset]];
-                if ($l1 === $l2) {
-                    $offset++;
-                } else if ($this->find_smudge($l1, $l2) && $part2 === (-1)) {
-                    $offset++;
-                    $part2 = 0;
-                    $part1 = -1;
-                } else if ($l1 !== $l2 && $part2 === 0) {
-                    $part2 = -1;
-                    $part1 = -1;
-                    $offset = 0;
-                    break;
-                }
+        // Run through all the lines, starting with the 2nd one so we don't have to make
+        // an out of bounds check for comparisons
+        for ($L = 1; $L < count($lines); $L++) {
+            // $c is the offset // $diff is the number of differences across the pattern we are testing // $i is the line we are testing
+            $c = 0;   $diff = 0;    $i = $L;
+            // Run through the pattern, incrementing the offset and testing in both directions until we find a difference or
+            // reach the end of the pattern. If we reach the boundary in either direction, we found a mirror pattern
+            while ($i - $c - 1 >= 0 && $i + $c < count($lines)) {
+                // XOR the two lines and count the number of differences
+                $diff += substr_count(decbin($lines[$i - $c - 1] ^ $lines[$i + $c]), '1');
+                // Continue the outer loop if we have more than one difference, skipping the rest of the inner loop
+                // as well as the rest of the outer loop
+                if ($diff > 1) continue 2;
 
+                $c++;
             }
-            if ($offset > 0) {
-                if ($part1 < 1) $part1 = $part1 === 0 ? $idx : 0;
-                if ($part2 < 1) $part2 = $part2 === 0 ? $idx : 0;
-                if ($part1 > 0 && $part2 > 0) return [$part1, $part2];
-            }
+            // If we are in the rows, multiply the line number by 100 to get the right answer increment.
+            $i = $dir < 1 ? $i * 100 : $i;
+            $part1 = $diff < 1 ? $i : $part1; // If we have no differences, we have found a pattern for part 1
+            $part2 = $diff === 1 ? $i : $part2; // If we have exactly one difference, we have found a pattern for part 2
+            if ($part1 > 0 && $part2 > 0) return [$part1, $part2]; // Return early if we have found both solutions
         }
-        echo 'Part 1 '. $part1 .', Part2 '. $part2 .''.PHP_EOL;
-        return [$part1, $part2];
-    }
 
-    protected function potential_mirrors($lines, $smudge = false) {
-        $potential = [];
-        foreach ($lines as $key => $line) {
-            if ($key - 1 < 0) continue;
-            if ($smudge && $this->find_smudge($lines[$key - 1], $line)) $potential[] = $key;
-            if ($line === $lines[$key - 1]) $potential[] = $key;
-        }
-        return $potential;
-    }
-
-    protected function find_smudge($str1, $str2) {
-        $temp = $str1;
-        $found = false;
-        for ($c = 0; $c < strlen($str1); $c++) {
-            $temp[$c] = $str1[$c] === '#' ? '.' : '#';
-            if ($temp === $str2) {
-                if ($found === true) return false;
-                else $found = true;
-            }
-            $temp[$c] = $str1[$c];
-        }
-        return $found;
+         // If we haven't found a solution yet, try the columns by incrementing $dir and recursing
+        if ($dir === 0)  return $this->find_mirror_pattern($pattern, ++$dir, $part1, $part2);
     }
 
 
@@ -143,33 +79,24 @@ class Day13 extends Day
      * @return array The parsed data
      */
     protected function parse_input($data) {
-        $data = preg_split('/\r\n|\r|\n/', $data);
+        $data = array_map(fn($block) =>
+            explode("\n", $block), explode("\n\n", $data)
+        );
 
-        // Split array into patterns, separated by empty lines
-        $patterns = [];
-        $pattern = 0;
-        foreach ($data as $key => $line) {
-            if (strlen($line) === 0) {
-                $pattern++;
-                continue;
-            }
-            $patterns[$pattern][] = $line;
-        }
-
-
+        $hash = ['.' => 0, '#' => 1];
         $out = [];
-        foreach ($patterns as $key => $pattern) {
-            $rows = [];
-            $cols = array_fill(0, strlen($pattern[0]), '');
+        foreach ($data as $key => $pattern) {
+            $rows = [];    $cols = [];
+            // Change all the "." to 0 and "#" to 1 and convert the binary strings to integers for easy diffing later
             foreach ($pattern as $key => $line) {
-                $rows[] = trim($line);
-                foreach (str_split($line) as $key => $char) {
-                    $cols[$key] .= $char;
-                }
+                $rows[] = str_replace(['.', '#'], ['0', '1'], $line);
+                for ($key = 0; $key < strlen($line); $key++)
+                    $cols[$key][] = $hash[$line[$key]];
             }
-            $out[] = ['rows' => $rows, 'cols' => $cols];
+            $rows = array_map('bindec', $rows);
+            $cols = array_map(fn($col) => bindec(implode('', $col)), $cols);
+            $out[] = [0 => $rows, 1 => $cols];
         }
-
         return $out;
     }
 }
